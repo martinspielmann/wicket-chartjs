6613fdc271467ee2c133a21b4dff93c8b1f996e8
Martin Spielmann <martin.spielmann@pingunaut.com>
2013-04-12 22:27:46 +0200

    Initial commit

---
+++ wicket-chartjs/src/test/java/com/pingunaut/wicket/chartjs/TestHomePage.java
@@ -0,0 +1,31 @@
+package com.pingunaut.wicket.chartjs;
+
+import junit.framework.TestCase;
+import org.apache.wicket.util.tester.WicketTester;
+
+import com.pingunaut.wicket.chartjs.example.HomePage;
+
+/**
+ * Simple test using the WicketTester
+ */
+public class TestHomePage extends TestCase
+{
+	private WicketTester tester;
+
+	public void setUp()
+	{
+		tester = new WicketTester();
+	}
+
+	public void testRenderMyPage()
+	{
+		//start and render the test page
+		tester.startPage(HomePage.class);
+
+		//assert rendered page class
+		tester.assertRenderedPage(HomePage.class);
+
+		//assert rendered label component
+		tester.assertLabel("message", "If you see this message wicket is properly configured and running");
+	}
+}
