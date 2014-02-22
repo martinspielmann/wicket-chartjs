248d0677e6e5fc79925cc88b4839182be38bc142
Martin Spielmann <martin.spielmann@pingunaut.com>
2014-02-22 15:00:00 +0100

    updated versioning, updated to wicket 6.14.0

--- wicket-chartjs/src/main/java/com/pingunaut/wicket/chartjs/data/PieChartData.java
+++ wicket-chartjs/src/main/java/com/pingunaut/wicket/chartjs/data/PieChartData.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2013 Martin Spielmann
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,8 +21,8 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 /**
  * The Class PieChartData provides chart data used by pie charts.
- * 
- * @author Martin Spielmann
+ *
+ * @author Martin Spielmann, Brill Pappin
  */
 @JsonInclude(Include.NON_NULL)
 public class PieChartData extends SimpleColorValueChartData {
@@ -33,7 +33,7 @@ public class PieChartData extends SimpleColorValueChartData {
 
 	/**
 	 * Instantiates a new pie chart data.
-	 * 
+	 *
 	 * @param value
 	 *            the value
 	 * @param color
