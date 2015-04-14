4ff7e44e822d088dd08bca447574e0607b4c4d97
Martin Spielmann <martin.spielmann@pingunaut.com>
2015-04-14 22:37:37 +0200

    updated to chartjs 1.0.2

--- src/main/java/com/pingunaut/wicket/chartjs/chart/impl/AbstractDataSetChart.java
+++ src/main/java/com/pingunaut/wicket/chartjs/chart/impl/AbstractDataSetChart.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2013 Martin Spielmann
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,46 +28,49 @@ import com.pingunaut.wicket.chartjs.options.AbstractChartOptions;
  * <li>{@link Line}</li>
  * <li>{@link Radar}</li>
  * </ul>
- * instead of simple color&value pairs, this type uses labels and datasets.
- * 
+ * instead of simple color and value pairs, this type uses labels and datasets.
+ *
  * @param <D>
- *            the generic type of chart data (has to extend
- *            {@link AbstractChartData}
+ *           the generic type of chart data (has to extend
+ *           {@link AbstractChartData}
  * @param <O>
- *            the generic type of chart options (has to extend
- *            {@link AbstractChartOptions}
+ *           the generic type of chart options (has to extend
+ *           {@link AbstractChartOptions}
  * @param <S>
- *            the generic type of a chart data set (has to extend
- *            {@link AbstractBaseDataSet}
- * 
+ *           the generic type of a chart data set (has to extend
+ *           {@link AbstractBaseDataSet}
+ *
  * @author Martin Spielmann
- * 
+ *
  */
-public abstract class AbstractDataSetChart<D extends AbstractChartData<S>, O extends AbstractChartOptions, S extends AbstractBaseDataSet> extends AbstractChart<O> implements IDataSetChart<D, O, S> {
+public abstract class AbstractDataSetChart<D extends AbstractChartData<S>, O extends AbstractChartOptions, S extends AbstractBaseDataSet>
+      extends AbstractChart<O>implements IDataSetChart<D, O, S> {
 
-	private static final long serialVersionUID = 999846601210465414L;
+   private static final long serialVersionUID = 999846601210465414L;
 
-	/** The data. */
-	protected D data;
+   /** The data. */
+   protected D data;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see com.pingunaut.wicket.chartjs.chart.IDataSetChart#getData()
-	 */
-	public D getData() {
-		return data;
-	}
+   /*
+    * (non-Javadoc)
+    * 
+    * @see com.pingunaut.wicket.chartjs.chart.IDataSetChart#getData()
+    */
+   @Override
+   public D getData() {
+      return data;
+   }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * com.pingunaut.wicket.chartjs.chart.IDataSetChart#setData(com.pingunaut
-	 * .wicket.chartjs.data.AbstractChartData)
-	 */
-	public void setData(D data) {
-		this.data = data;
-	}
+   /*
+    * (non-Javadoc)
+    * 
+    * @see
+    * com.pingunaut.wicket.chartjs.chart.IDataSetChart#setData(com.pingunaut
+    * .wicket.chartjs.data.AbstractChartData)
+    */
+   @Override
+   public void setData(final D data) {
+      this.data = data;
+   }
 
 }
--- src/main/java/com/pingunaut/wicket/chartjs/example/ExamplePage.java
+++ src/main/java/com/pingunaut/wicket/chartjs/example/ExamplePage.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2013 Martin Spielmann
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,52 +31,57 @@ import com.pingunaut.wicket.chartjs.data.sets.LineDataSet;
 /**
  * The Class ExamplePage provides a tiny little running example of
  * wicket-chartjs. to use it, just type mvn jetty:run
- * 
+ *
  * @author Martin Spielmann
  */
 public class ExamplePage extends WebPage {
 
-	private static final long serialVersionUID = -8484356423313672843L;
+   private static final long serialVersionUID = -8484356423313672843L;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.apache.wicket.Component#onInitialize()
-	 */
-	@Override
-	protected void onInitialize() {
-		super.onInitialize();
-		LineChartPanel lineChartPanel = new LineChartPanel("lineChart", Model.of(new Line()));
-		add(lineChartPanel);
-		PieChartPanel pieChartPanel = new PieChartPanel("pieChart", Model.of(new Pie()));
-		add(pieChartPanel);
+   /*
+    * (non-Javadoc)
+    *
+    * @see org.apache.wicket.Component#onInitialize()
+    */
+   @Override
+   protected void onInitialize() {
+      super.onInitialize();
+      LineChartPanel lineChartPanel = new LineChartPanel("lineChart", Model.of(new Line()));
+      add(lineChartPanel);
+      PieChartPanel pieChartPanel = new PieChartPanel("pieChart", Model.of(new Pie()));
+      add(pieChartPanel);
 
-		List<String> labels = new ArrayList<String>();
-		labels.add("jan");
-		labels.add("feb");
-		labels.add("mar");
-		labels.add("apr");
+      PieChartPanel pieChartWithoutTooltips = new PieChartPanel("pieChartWithoutTooltips", Model.of(new Pie()));
+      add(pieChartWithoutTooltips);
 
-		List<Integer> values1 = new ArrayList<Integer>();
-		values1.add(4);
-		values1.add(2);
-		values1.add(6);
-		values1.add(7);
+      List<String> labels = new ArrayList<String>();
+      labels.add("jan");
+      labels.add("feb");
+      labels.add("mar");
+      labels.add("apr");
 
-		lineChartPanel.getChart().getData().getDatasets().add(new LineDataSet(values1));
-		lineChartPanel.getChart().getData().getLabels().addAll(labels);
-		
-		lineChartPanel.getChart().getOptions().setCustomTooltips(
-		  "function(tooltip){var tooltipEl = $('#chartjs-tooltip');"+
-		  "if (!tooltip) {tooltipEl.css({opacity: 0});return;}"+
-		  "tooltipEl.removeClass('above below');tooltipEl.addClass(tooltip.yAlign);"+
-		  "tooltipEl.html('my custom text: '+tooltip.text);var top;if (tooltip.yAlign == 'above') {top = tooltip.y - tooltip.caretHeight - tooltip.caretPadding;"+
-	    "} else {top = tooltip.y + tooltip.caretHeight + tooltip.caretPadding;}tooltipEl.css({"+
-	    "opacity: 1,left: tooltip.chart.canvas.offsetLeft + tooltip.x + 'px',top: tooltip.chart.canvas.offsetTop + top + 'px',"+
-	    "fontFamily: tooltip.fontFamily,fontSize: tooltip.fontSize,fontStyle: tooltip.fontStyle,});}");
-		
-		for (Integer i : values1) {
-			pieChartPanel.getChart().getData().add(new PieChartData(i, "#" + i + i + i));
-		}
-	}
+      List<Integer> values1 = new ArrayList<Integer>();
+      values1.add(4);
+      values1.add(2);
+      values1.add(6);
+      values1.add(7);
+
+      lineChartPanel.getChart().getData().getDatasets().add(new LineDataSet(values1));
+      lineChartPanel.getChart().getData().getLabels().addAll(labels);
+
+      lineChartPanel.getChart().getOptions()
+            .setCustomTooltips("function(tooltip){var tooltipEl = $('#chartjs-tooltip');"
+                  + "if (!tooltip) {tooltipEl.css({opacity: 0});return;}"
+                  + "tooltipEl.removeClass('above below');tooltipEl.addClass(tooltip.yAlign);"
+                  + "tooltipEl.html('my custom text: '+tooltip.text);var top;if (tooltip.yAlign == 'above') {top = tooltip.y - tooltip.caretHeight - tooltip.caretPadding;"
+                  + "} else {top = tooltip.y + tooltip.caretHeight + tooltip.caretPadding;}tooltipEl.css({"
+                  + "opacity: 1,left: tooltip.chart.canvas.offsetLeft + tooltip.x + 'px',top: tooltip.chart.canvas.offsetTop + top + 'px',"
+                  + "fontFamily: tooltip.fontFamily,fontSize: tooltip.fontSize,fontStyle: tooltip.fontStyle,});}");
+
+      for (Integer i : values1) {
+         pieChartPanel.getChart().getData().add(new PieChartData(i, "#" + i + i + i));
+         pieChartWithoutTooltips.getChart().getData().add(new PieChartData(i, "#" + (i + 3) + i + i));
+         pieChartWithoutTooltips.getChart().getOptions().setShowTooltips(Boolean.FALSE);
+      }
+   }
 }
