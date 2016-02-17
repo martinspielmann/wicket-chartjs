0017f8905439554e42df5b90786a5526008643ef
Martin Spielmann <martin.spielmann@pingunaut.com>
2016-02-17 12:35:14 +0100

    fixed tests

--- src/test/java/com/pingunaut/wicket/chartjs/core/panel/BarChartPanelTest.java
+++ src/test/java/com/pingunaut/wicket/chartjs/core/panel/BarChartPanelTest.java
@@ -3,15 +3,18 @@ package com.pingunaut.wicket.chartjs.core.panel;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.util.tester.WicketTestCase;
+import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Test;
 
 import com.pingunaut.wicket.chartjs.chart.impl.Bar;
 
-public class BarChartPanelTest extends WicketTestCase{
+public class BarChartPanelTest {
 
+	WicketTester tester = new WicketTester();
+	
 	@Test
 	public void testBarChartPanelStringIModelOfQextendsBarIntInt() {
+		
 		IModel<? extends Bar> model = Model.of(new Bar());
 		BarChartPanel panel = new BarChartPanel("foo", model , 50, 60);
 		tester.startComponentInPage(panel);
--- src/test/java/com/pingunaut/wicket/chartjs/core/panel/DoughnutChartPanelTest.java
+++ src/test/java/com/pingunaut/wicket/chartjs/core/panel/DoughnutChartPanelTest.java
@@ -1,17 +1,16 @@
 package com.pingunaut.wicket.chartjs.core.panel;
 
-import static org.junit.Assert.*;
-
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.util.tester.WicketTestCase;
+import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Test;
 
-import com.pingunaut.wicket.chartjs.chart.impl.Bar;
 import com.pingunaut.wicket.chartjs.chart.impl.Doughnut;
 
-public class DoughnutChartPanelTest extends WicketTestCase{
+public class DoughnutChartPanelTest{
+
+	WicketTester tester = new WicketTester();
 
 	@Test
 	public void testDoughnutChartPanelStringIModelOfQextendsDoughnutIntInt() {
